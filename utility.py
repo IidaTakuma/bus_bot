e73caf93321aed74cff76ab93246a3b8db82daa3
IidaTakuma <ta9maxxx@gmail.com>
2021-02-12 07:51:22 +0900

    fix

--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 from functools import reduce
+from collections import OrderedDict
 from pytz import timezone
 import datetime
 import jpholiday
@@ -7,10 +8,12 @@ import jpholiday
 def make_response_text(mode):
     timeTable = selectTimeTable(mode)
     dict_key = make_dict_key()
-    return calc_feture_bus_times(timeTable, dict_key)
+    print("now key is", dict_key)
+    times = calc_feture_bus_times(timeTable, dict_key)
+    return times[0] + "\n" + times[1] + "\n" + times[2]
 
 
-def calc_feture_bus_times(timetable: dict, now: int) -> tuple:
+def calc_feture_bus_times(timetable: OrderedDict, now: int) -> tuple:
     times = [time for time in timetable.keys()]
     idx = reduce(lambda a, x: a+1 if x < now else a, times, 0)
     ret = [timetable[k] for k in times[idx:idx+3]]
@@ -34,14 +37,19 @@ def selectTimeTable(mode):
     jst_now = datetime.datetime.now(timezone('Asia/Tokyo'))
     is_holiday = jpholiday.is_holiday(jst_now)
     if is_holiday:
+        print("today is holiday")
         return diagram.sunday
     elif jst_now.weekday() < 5:
+        print("today is weekday")
         return diagram.weekday
     elif jst_now.weekday() == 5:
+        print("today is saturday")
         return diagram.saturday
     elif jst_now.weekday() == 6:
+        print("today is sunday")
         return diagram.sunday
     else:
+        print("day select error")
         pass
 
 
