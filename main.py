4f873c97b446ba9d22fdf62785d940a08ff9845f
IidaTakuma <ta9maxxx@gmail.com>
2021-02-14 05:04:52 +0900

    refactor

--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 import settings
 import json
 
-from typing import (
-    Optional,
-)
-from fastapi import (
-    FastAPI,
-    Header,
-    HTTPException,
-    Request,
-)
+from typing import (Optional,)
+from fastapi import (FastAPI, Header, HTTPException, Request,)
 
 from linebot import (LineBotApi, WebhookParser,)
 from linebot.exceptions import (InvalidSignatureError, LineBotApiError,)
-from linebot.models import (MessageEvent, TextSendMessage,)
+from linebot.models import (TextSendMessage,)
 
-from utility import make_response_text
+from utility import TimeTableUtility
 
 
 CHANNEL_ACCESS_TOKEN = settings.CAT
 CHANNEL_SECRET = settings.CS
 
 app = FastAPI()
-
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 webhock_parser = WebhookParser(CHANNEL_SECRET)
-
-
-@app.get("/echo")
-async def echo():
-    return {"message": "hello!!"}
 
 
 @app.post("/callback")
@@ -46,12 +33,11 @@ async def callback(
     except LineBotApiError:
         raise HTTPException(status_code=400, detail="Line bot api error")
 
+    # モードは "TakatsukiToKansai","TondaToKansai","KansaiToTakatsuki","KansaiToTonda"のいずれか
     for event in events:
         mode = json.loads(str(event))['postback']['data']
-        print(mode)
         if mode is not None:
-            # モードは「TakatsukiToKansai」「TondaToKansai」「KansaiToTakatsuki」「KansaiToTonda」のどれか
-            ret_text = make_response_text(mode)
+            ret_text = TimeTableUtility.make_response_text(mode)
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text=ret_text)
--- utility.py
+++ utility.py
@@ -5,59 +5,63 @@ import datetime
 import jpholiday
 
 
-def make_response_text(mode):
-    timeTable = selectTimeTable(mode)
-    dict_key = make_dict_key()
-    print("now key is", dict_key)
-    times = calc_feture_bus_times(timeTable, dict_key)
-    return times[0] + "\n" + times[1] + "\n" + times[2]
+class TimeTable:
+    def __init__(self, _distination, _diagram):
+        self.distination = _distination
+        self.diagram = _diagram
+        self.feture_schedule = self.calc_feture_schedule()
+
+    def calc_feture_schedule(self) -> tuple:
+        times = [time for time in self.diagram.keys()]
+        dict_key = self.make_dict_key()
+        idx = reduce(lambda a, x: a+1 if x < dict_key else a, times, 0)
+        ret = [self.diagram[k] for k in times[idx:idx+3]]
+        while len(ret) < 3:
+            ret.append("運行終了")
+        return tuple(ret)
+
+    def make_dict_key() -> str:
+        jst_now = datetime.datetime.now(timezone('Asia/Tokyo'))
+        return jst_now.hour * 60 + jst_now.minute
 
 
-def calc_feture_bus_times(timetable: OrderedDict, now: int) -> tuple:
-    times = [time for time in timetable.keys()]
-    idx = reduce(lambda a, x: a+1 if x < now else a, times, 0)
-    ret = [timetable[k] for k in times[idx:idx+3]]
-    while len(ret) < 3:
-        ret.append("運行終了")
-    return tuple(ret)
+class TimeTableUtility:
+    @classmethod
+    def selectTimeTable(mode: str) -> TimeTable:
+        if mode == "TakatsukiToKansai":
+            from timetable import TAKATSUKI_TO_KANSAI as diagram
+            distination = "JR高槻駅 -> 関西大学"
+        elif mode == "TondaToKansai":
+            from timetable import TONDA_TO_KANSAI as diagram
+            distination = "JR富田駅 -> 関西大学"
+        elif mode == "KansaiToTakatsuki":
+            from timetable import KANSAI_TO_TAKATSUKI as diagram
+            distination = "関西大学 -> JR高槻駅"
+        elif mode == "KansaiToTonda":
+            from timetable import KANSAI_TO_TONDA as diagram
+            distination = "関西大学 -> JR富田駅"
+        else:
+            pass
 
+        jst_now = datetime.datetime.now(timezone('Asia/Tokyo'))
+        is_holiday = jpholiday.is_holiday(jst_now)
+        if is_holiday or jst_now.weekday() == 6:
+            return TimeTable(distination, diagram.sunday)
+        elif jst_now.weekday() < 5:
+            return TimeTable(distination, diagram.weekday)
+        elif jst_now.weekday() == 5:
+            return TimeTable(distination, diagram.saturday)
+        else:
+            pass
 
-def selectTimeTable(mode):
-    if mode == "TakatsukiToKansai":
-        from timetable import TAKATSUKI_TO_KANSAI as diagram
-    elif mode == "TondaToKansai":
-        from timetable import TONDA_TO_KANSAI as diagram
-    elif mode == "KansaiToTakatsuki":
-        from timetable import KANSAI_TO_TAKATSUKI as diagram
-    elif mode == "KansaiToTonda":
-        from timetable import KANSAI_TO_TONDA as diagram
-    else:
-        pass
+    @classmethod
+    def make_response_text(mode: str) -> str:
+        return TimeTableUtility.make_text(TimeTableUtility.selectTimeTable(mode))
 
-    jst_now = datetime.datetime.now(timezone('Asia/Tokyo'))
-    is_holiday = jpholiday.is_holiday(jst_now)
-    if is_holiday:
-        print("today is holiday")
-        return diagram.sunday
-    elif jst_now.weekday() < 5:
-        print("today is weekday")
-        return diagram.weekday
-    elif jst_now.weekday() == 5:
-        print("today is saturday")
-        return diagram.saturday
-    elif jst_now.weekday() == 6:
-        print("today is sunday")
-        return diagram.sunday
-    else:
-        print("day select error")
-        pass
-
-
-def make_dict_key():
-    jst_now = datetime.datetime.now(timezone('Asia/Tokyo'))
-    return jst_now.hour * 60 + jst_now.minute
-
-
-if __name__ == '__main__':
-    make_response_text("TakatsukiToKansai")
-    # print(calc_feture_bus_times.fill_gte_3.map(lambda x: x.toDisp())
+    @classmethod
+    def make_text(timeTable: TimeTable) -> str:
+        text = "次のバスの時刻は\n" \
+            + "[1]:" + timeTable.feture_schedule[0] + "\n" \
+            + "[2]:" + timeTable.feture_schedule[1] + "\n" \
+            + "[3]:" + timeTable.feture_schedule[2] + "\n"
+        return text
