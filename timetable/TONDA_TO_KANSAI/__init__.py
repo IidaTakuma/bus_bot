3e8325e2e35b96551c9c6ee523148e5475f02af9
IidaTakuma <ta9maxxx@gmail.com>
2021-03-09 15:48:53 +0900

    delete timeTable

--- timetable/TONDA_TO_KANSAI/__init__.py
+++
@@ -1,3 +0,0 @@
-from .weekday import weekday
-from .saturday import saturday
-from .sunday import sunday
--- timetable/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import TAKATSUKI_TO_KANSAI
-from . import TONDA_TO_KANSAI
-from . import KANSAI_TO_TAKATSUKI
-from . import KANSAI_TO_TONDA
